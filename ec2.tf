722d1b38b130965350f586e8ca64ea4d934bb424
Akshat kay <88634496+Chanarra@users.noreply.github.com>
2025-04-06 16:09:46 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,4 @@
 resource "aws_instance" "myec2" {
-  ami                     = "ami-00a929b66ed6e0de6"
+  ami                     = "ami-084568db4383264d4"
   instance_type           = "t2.micro"
-  tags={
-    name = "hcp_ec2"
-  }
 }
